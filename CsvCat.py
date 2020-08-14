96565c00f350f50fde586dec71f6638712e8fa25
just-satoshi <hankju91@gmail.com>
2020-08-14 11:28:30 +0800

    fix bug

---
+++ CsvCat.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 10:15:24 2020
+
+@author: Administrator
+"""
+import csv
+import os 
+
+i=0
+j = 0
+path='HotelImage'
+
+
+imp = open('C:\\Users\Administrator\Desktop\\'+path+'.csv', 'r',encoding="utf-8")
+#out = open('mycsv.csv' , 'wb')
+#writer = csv.writer(out)
+myCsv = csv.reader(imp, quoting=csv.QUOTE_NONE, quotechar=' ', delimiter=',')
+if not os.path.isdir('C:\\Users\Administrator\Desktop\\'+path):
+    os.mkdir('C:\\Users\Administrator\Desktop\\'+path)
+for row in myCsv:
+        if i == 0:
+            out = open('C:\\Users\Administrator\Desktop\\'+path+'\\'+path+str(j)+'.csv','a+',encoding="utf-8", newline='')
+            writer = csv.writer(out)
+        else:     
+            writer.writerow(row)
+            #print(str(j)+':'+str(i))
+            
+        if i == 29999:
+            i = -1
+            j += 1
+        i+=1
+
+print(j)
+imp.close()
+out.close()
--- rewrite(AllListfile).py
+++ rewrite(AllListfile).py
@@ -8,7 +8,8 @@ This is a temporary script file.
 import os 
 import csv
 import zipfile
-
+filenameDB ='path'
+filename='path'
 #壓縮
 def Achive_Folder_To_ZIP(sFilePath):
     """
@@ -26,47 +27,48 @@ def Achive_Folder_To_ZIP(sFilePath):
             zf.write(aFile)
     zf.close()
 
+def CsvCat(filenameDB,filename):
+    if not os.path.isdir(filenameDB):
+        os.mkdir(filenameDB)
+    for root, dirs, files in os.walk(filename):
+        #for name in dirs:
+        #    print("目錄--->",os.path.join(root, name))
+        for name in files:
+            i = 0
+            print("檔案--->",os.path.join(root, name))
+            f = open(os.path.join(root, name),encoding="utf-8")
+            myCsv = csv.reader(f, delimiter='|', quoting=csv.QUOTE_NONE, quotechar=' ')
+            #myCsv = csv.reader(f, quoting=csv.QUOTE_NONE, quotechar=' ')
+            headers = next(myCsv)
+            f2 = open(filenameDB+name.replace('.csv', '.json'),'a+',encoding="utf-8")
+            f2.write('[')
+            for rows in myCsv:  
+                i+=1
+                #Debug
+                #print(name+':'+str(i))
+                if rows[0]== '' :
+                    print(os.path.join(root, name)+':'+str(i)+":null")
+                else:                       
+                    #CsvRemoveReplace
+                    row = [item.replace(r'"', r'\"').replace('，', ',').replace('\t','') for item in rows]    
+                    #DBFunction
+                    DBFunction = ()
+                    #print('\033[1;3'+str(i%8)+'m'+name+':'+str(i)+':'+str(rows[0])+'\033[0m')
+                    if i!=1: f2.write(',')
+                    
+                    #writeDBFunction
+                    f2.write(DBFunction)   
 
-filenameDB ='path'
-filename='path'
+            f2.write(']')  
+            f2.close()  
+            f.close()
+        #print(len(df))
 
-if not os.path.isdir(filenameDB):
-    os.mkdir(filenameDB)
-for root, dirs, files in os.walk(filename):
-    #for name in dirs:
-    #    print("目錄--->",os.path.join(root, name))
-    for name in files:
-        i = 0
-        print("檔案--->",os.path.join(root, name))
-        f = open(os.path.join(root, name),encoding="utf-8")
-        myCsv = csv.reader(f, delimiter='|', quoting=csv.QUOTE_NONE, quotechar=' ')
-        #myCsv = csv.reader(f, quoting=csv.QUOTE_NONE, quotechar=' ')
-        headers = next(myCsv)
-        f2 = open(filenameDB+name.replace('.csv', '.json'),'a+',encoding="utf-8")
-        f2.write('[')
-        for rows in myCsv:  
-            i+=1
-            #Debug
-            #print(name+':'+str(i))
-            if rows[0]== '' :
-                print(os.path.join(root, name)+':'+str(i)+":null")
-            else:                       
-                #CsvRemoveReplace
-                row = [item.replace(r'"', r'\"').replace('，', ',').replace('\t','') for item in rows]    
-                #DBFunction
-                DBFunction = ()
-                #print('\033[1;3'+str(i%8)+'m'+name+':'+str(i)+':'+str(rows[0])+'\033[0m')
-                if i!=1: f2.write(',')
-                
-                #writeDBFunction
-                f2.write(DBFunction)   
 
-        f2.write(']')  
-        f2.close()  
-        f.close()
-    #print(len(df))
-        
-#壓縮路徑
-Achive_Folder_To_ZIP(filenameDB)     
+if __name__ == '__main__': 
+
+    CsvCat(filenameDB,filename)      
+    #壓縮路徑
+    Achive_Folder_To_ZIP(filenameDB)     
         
  
